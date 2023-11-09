bed2dd4016570a6aa1e981f30cba94e636a4b583
Mohamed Mellouky <contact.mellouky@gmail.com>
2023-11-09 15:29:21 +0100

    feat: add full behaviour

--- essaim_robot/drone.gd
+++ essaim_robot/drone.gd
@@ -1,5 +1,9 @@
 extends RigidBody3D
 
+var timer:int = 0
+const  BATTERY_CHANGE_INTERVAL:int = 5
+const BATTERY_LOW_VALUE:int = 20
+var is_battery_low:bool = false 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,13 +12,43 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# UpdateBatteryLevel()
+#	print("Drones x position: ", self.position.x)
+#	print("Drones y position: ", self.position.y)
+#	print("Drones z position: ", self.position.z)
 	pass
 
+func UpdateBatteryLevel() : 
+	var battery_level:int = self.get_meta("battery")
+#	print("battery level value: ", battery_level)
+	if timer >= BATTERY_CHANGE_INTERVAL : 
+		battery_level -= 1
+		self.set_meta("battery", battery_level)
+		timer = 0
+		print("battery level changed: ", battery_level)
+#		if battery_level <= BATTERY_LOW_VALUE : 
+#			is_battery_low = true 
+#			self.set_meta("is_battery_low", is_battery_low)
+#			print("Battery is low")
+#			# Il faut revenir à la base
+#			GoToBase()
+#			recharge()
+#			self.translate(Vector3(self.get_meta("baseXpos"), 
+#							self.get_meta("baseYpos"), 
+#							1))
+	else :
+		timer += 1
+		print("timer value ", timer)
 
 
-	
-
-
+func GoToBase() : 
+		print("Go to base position (", self.get_meta("baseXpos"), ", ", self.get_meta("baseYpos"), ", ", self.get_meta("baseZpos"), ")")
+		self.position.x = self.get_meta("baseXpos")
+		self.position.y = self.position.y 
+		self.position.z = self.get_meta("baseZpos")
+		
+func recharge() : 
+	self.set_meta("battery", 100)
 func _on_body_entered(tree):
 #	var needs_water:int = tree.get_meta("withered")
 #	var need_fertilizer:int = tree.get_meta("need_fertilizer")
